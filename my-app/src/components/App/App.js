f17b73363329cb382ea33b93ab9ebace25956189
Theo Martin Rosero Mosquera <theomrosero@gmail.com>
2022-01-23 22:10:43 -0500

    Llamada a componente Login

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from '../Homepage/Homepage';
 import User from '../User/User';
 import '../../styles/index.css';
+import Login from '../Homepage/Login';
 
 function App (){
     return (
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/user" element={<User />} />
+                <Route path="/" element={<Login />} />
             </Routes>
     );
 }
