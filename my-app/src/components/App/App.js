195d9d1250e0a973ec6e6e55c8519ff5b2ea3cc4
kevin chuquimarca <santi.kevin0936@gmail.com>
2022-01-24 10:28:32 -0500

    componentes para login

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import Homepage from '../Homepage/Homepage';
 import User from '../User/User';
+import Login from '../Login/Login';
 import '../../styles/index.css';
-import Login from '../Login/Login'
 
 
 function App (){
@@ -12,7 +13,6 @@ function App (){
                 <Route path="/" element={<Homepage />} />
                 <Route path="/user" element={<User />} />
                 <Route path="/login" element={<Login />} />
-
             </Routes>
     );
 }
